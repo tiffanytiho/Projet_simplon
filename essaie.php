857edee822b86a6e508bb4b6be02da7cb011b5f4
tiffanytiho <135434318+tiffanytiho@users.noreply.github.com>
2023-06-03 17:40:10 +0000

    deuxieme copie
    -j'ai amélioré le retour des erreurs
    -j'ai affiché les informations se trouvant dans la base de donnée
    -j'ai compris pourquoi certaines saisies ne s'enregistrais pas

--- essaie.php
+++ essaie.php
@@ -2,19 +2,54 @@
 <html>
 <head>
 	<title></title>
+	<style type="text/css">
+        body
+        {
+            display: flex;
+            flex-direction:column;
+            justify-content:center;
+            align-items:center ;
+        }
+	</style>
 </head>
 <body>
 	<center><h1> La liste des participants </h1></center>
 </body>
-<?php
-	try{
-		$con= new PDO("mysql:host=localhost;dbname=fromdonnees;port=3306;charset=utf8",'root','');
-	}catch(Exception $e) {
-		echo 'Erreur de connexion :'. $e->getMessage();
-	}
-	$formulaire = $con->query('SELECT * from users');
-	echo'<table>';
-	while($ligne = $formulaire->fetch()){
+	<?php
+		try{
+			$con= new PDO("mysql:host=localhost;dbname=fromdonnees;port=3306;charset=utf8",'root','');
+			}catch(Exception $e) {
+				echo 'Erreur de connexion :'. $e->getMessage();
+			}
+			$formulaire = $con->query('SELECT * from users');
+			/*echo'<table>';*/
+	?>
+
+	<table class="deco">
+		<tr>
+			<td> Nom </td>
+			<td> Prénoms </td>
+			<td> Numéro </td>
+			<td> Adresse </td>
+		</tr>
+		<?php
+			while ( $ligne = $formulaire->fetch()) 
+			{
+				/*
+				echo "<pre>";
+				var_dump($ligne);
+				echo "</pre>";*/
+				?>
+				<tr>
+					<td> <?php echo $ligne["nom"]?> </td>
+					<td> <?php echo $ligne["prenom"]?> </td>
+					<td> <?php echo $ligne["numero"]?> </td>
+					<td> <?php echo $ligne["adresse"]?> </td>
+				</tr>
+			<?php } ?>
+	</table>
+	
+	<!--while($ligne = $formulaire->fetch()){
 		echo '<tr>';
 			echo'<td>';
 				echo $ligne['nom'];
@@ -33,6 +68,5 @@
 			echo'</td>';
 		echo '</tr>';
 	}
-	echo'</table>';
-?>
+	echo'</table>';-->
 </html>
--- liste_presence.php
+++ liste_presence.php
@@ -1,51 +1,72 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Liste de présence</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="projetAWS.css">
-    <script src="main.js"></script>
-</head>
-<body>
-    <form action="" method="POST">
-        <label for=""> Nom : </label>
-        <input type="text" name="nom">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title> Formulaire de présence</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script src="main.js"></script>
 
-        <label for=""> Prénoms : </label>
-        <input type="text" name="prenom">
-
-        <label for=""> Numéro : </label>
-        <input type="text" name="numero">
-
-        <label for=""> Adresse : </label>
-        <input type="text" name="adresse">
-
-        <input type="submit" value="ENVOYER" name="envoyer">
-    </form>
-
-    <?php
-        $con = mysqli_connect('localhost','root','');
-        if(!$con){
-            echo"La connexion a échoué";
-        }if(!mysqli_select_db($con,'fromdonnees'))
+        <style type="text/css">
+           body
             {
-                echo"La connexion a échoué";
-            }if(isset($_POST['nom']) AND isset($_POST['prenom']) AND isset($_POST['numero']) AND isset($_POST['adresse']) AND isset($_POST['envoyer'])){
-                $nom = $_POST['nom'];
-                $prenom = $_POST['prenom'];
-                $numero = $_POST['numero'];
-                $adresse = $_POST['adresse'];
-                //echo $nom ;
-                
-                $sql= "INSERT INTO users(nom,prenom,numero,adresse) VALUES ('$nom','$prenom','$numero','$adresse')";
-                if(!mysqli_query($con,$sql)) {
-                    echo "Nous attendons les informations  ";
-            } else{
-                echo "Les informations ont été saisies";
+                display: flex;
+                flex-direction:column;
+                justify-content:center;
+                align-items:center ;
             }
-        }
-    ?>
-</body>
+            form
+            {
+                display:grid;
+                padding:2em; 
+            }
+            form input, label
+            {
+                margin:0,2em;
+                padding:0,2em;
+            }
+        </style>
+    </head>
+    <body>
+        <form action="" method="POST">
+            <label for="" class="form"> Nom : </label>
+            <input type="text" name="nom">
+
+            <label for="" class="form"> Prénoms : </label>
+            <input type="text" name="prenom">
+
+            <label for="" class="form"> Numéro : </label>
+            <input type="text" name="numero">
+
+            <label for="" class="form"> Adresse : </label>
+            <input type="text" name="adresse">
+
+            <input type="submit" value="ENVOYER" name="envoyer" class="boton">
+        </form>
+
+        <?php
+            $con = mysqli_connect('localhost','root','');
+            if(!$con){
+                echo"La connexion a échoué";
+            }
+            if(!mysqli_select_db($con,'fromdonnees'))
+                {
+                    echo"La connexion a échoué";
+                }if(isset($_POST['nom']) AND isset($_POST['prenom']) AND isset($_POST['numero']) AND isset($_POST['adresse']) AND isset($_POST['envoyer'])){
+                    $nom = $_POST['nom'];
+                    $prenom = $_POST['prenom'];
+                    $numero = $_POST['numero'];
+                    $adresse = $_POST['adresse'];
+                    //echo $nom ;
+                    
+                        $sql= "INSERT INTO users(nom,prenom,numero,adresse) VALUES ('$nom','$prenom','$numero','$adresse')";
+                    if(!mysqli_query($con,$sql)) {
+                        echo "La requete non execute  ";
+                } else{
+                    echo "Nous attendons votre saisie ou" ;
+                    echo "<br> Le numéro a été mal saisi ";
+                }
+            }
+        ?>
+    </body>
 </html>
